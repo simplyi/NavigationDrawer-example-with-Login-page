b3a6e4f40adfaf223a07380ab9591b5ec205f70a
Sergey Kargopolov <sergeykargopolov@Sergeys-MacBook-Pro.local>
2015-05-23 18:22:07 -0700

    first commit

---
+++ MyNavigationDrawer/MyCustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCustomTableViewCell.swift
+//  MyNavigationDrawer
+//
+//  Created by Sergey Kargopolov on 2015-03-21.
+//  Copyright (c) 2015 Sergey Kargopolov. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
